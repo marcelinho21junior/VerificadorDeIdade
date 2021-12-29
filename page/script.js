23e2502d9945ee65884adab9255c3f2641dd1560
marcelinho21junior <marcelo.belitardo@fatec.sp.gov.br>
2021-12-28 23:14:51 -0300

    commit atuualizado 21

--- page/script.js
+++ page/script.js
@@ -8,7 +8,43 @@ function verificar(){
         window.alert('ERRO, verifique os dados e tente novamente!')
         res.innerHTML = `UNDEFINED`
     }else{
+        var img = document.getElementById("img")
         var idade = ano - (nasc.value)
-        res.innerHTML = `Idade ${idade} anos`
+        var fsex = document.getElementsByName("radsex")
+        var genero = ''
+        if(fsex[0].checked){
+            genero = 'Homem'
+            if(idade >=0 && idade < 10){
+                //criança
+                img.setAttribute('src','')
+            }else if(idade >= 21){
+                //jovem
+                img.setAttribute('src','')
+            }else if(idade >= 49){
+                //adulto
+                img.setAttribute('src','')
+            }else{
+                //idoso
+                img.setAttribute('src','')
+            }
+
+        }else if(fsex[1].checked){
+            genero='Mulher'
+            if(idade >=0 && idade < 10){
+                //criança
+                img.setAttribute('src','')
+            }else if(idade >= 21){
+                //jovem
+                img.setAttribute('src','')
+            }else if(idade >= 49){
+                //adulto
+                img.setAttribute('src','')
+            }else{
+                //idoso
+                img.setAttribute('src','')
+            }
+        }
+        
+        res.innerHTML = `Detectamos ${genero} com Idade: ${idade} anos`
     }
 }
